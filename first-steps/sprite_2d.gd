400218a0297158384d0dabe73e27f00e5c9e06c5
Miray_Black <1lonelybrony1@gmail.com>
2025-08-30 01:17:14 +0300

    Walk player

--- first-steps/sprite_2d.gd
+++ first-steps/sprite_2d.gd
@@ -1,18 +1,17 @@
 extends CharacterBody2D
 
+# Общая скорость движения
 var speed = 200
-var gravity = 980
-
-func _ready():
-	# Эта функция вызывается при создании нода
-	print("Player node is ready!")
 
 func _physics_process(delta):
-	# Движение вправо с постоянной скоростью
-	velocity.x = speed
+	# Получаем направление движения
+	var direction_x = Input.get_axis("move_left", "move_right")
+	var direction_y = Input.get_axis("move_up", "move_down")
+	var shift = Input
 	
-	# Применяем гравитацию
-	velocity.y += gravity * delta
+	# Устанавливаем скорость в обоих направлениях
+	velocity.x = speed * direction_x
+	velocity.y = speed * direction_y
 	
 	# Применяем движение
 	move_and_slide()
